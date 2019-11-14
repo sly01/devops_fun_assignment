6d796951b1c85fc86ead8043197820c723f71018
sly01 <aerkoc01@gmail.com>
2019-11-14 16:57:48 +0100

    Added unit test for python lambda functions

---
+++ lambda_functions/hello1/main_test.py
@@ -0,0 +1,5 @@
+import unittest
+from main import index
+class TestFunctionHello1(unittest.TestCase):
+    def test_return_content_successfull(self):
+        self.assertTrue(index('','')['statusCode'] is 200 and "Hello World 1!" in index('','')['body'])
